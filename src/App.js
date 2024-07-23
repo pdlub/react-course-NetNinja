13837eef1ae537e48c4937d6ef8477107bdcc9d6
shanker <uperbacker@gmail.com>
2024-07-23 20:52:14 +0800

    add navbar component, add home component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
+
 
 function App() {
   const title = 'Welcome to the new blog';
   const likes = 50;
 
-  const link = 'https://www.google.com';
 
 
   return (
     <div className="App">
+      <Navbar/>
       <div className="content">
-        <h1>{title}</h1>
-        <p>Liked {likes} times</p>
-        <p>{[2, 'om', 3, 7, 'polt']}</p>
-        <a href={link}>google link</a>
+        <Home/>
+
       </div>
     </div>
   );
