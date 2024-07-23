8267efdb8634d53246b62de7fc8b5d87526aeb6b
shanker <uperbacker@gmail.com>
2024-07-23 21:25:31 +0800

    add Click Me Again button

--- src/Home.js
+++ src/Home.js
@@ -4,10 +4,15 @@ const Home = () => {
         console.log('hello, ninjas');
     }
 
+    const handleClickAgain = name => {
+        console.log('hello ' + name);
+    }
+
     return ( 
         <div className="home">
             <h2>Homepage</h2>
-            <button onClick={handleClick}>Click me</button>  
+            <button onClick={handleClick}>Click me</button> 
+            <button onClick={() => handleClickAgain('mario')}>Click me again</button> 
         </div>
      );
 }
