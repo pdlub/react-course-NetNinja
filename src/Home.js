6eec4523bd40b7eecbbbd9249fd2eb13993bf7f2
shanker <uperbacker@gmail.com>
2024-07-26 00:59:05 +0800

    add mario blogs component in BlogList

--- src/Home.js
+++ src/Home.js
@@ -11,7 +11,8 @@ const Home = () => {
 
     return ( 
         <div className="home">
-            <BlogList blogs = {blogs}/>
+            <BlogList blogs = {blogs} title = {'All blogs'}/>
+            <BlogList blogs = {blogs.filter((blog) => blog.author === 'mario')} title={"Mario's blog"}/>
 
         </div>
      );
