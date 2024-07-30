7a1d6c7c3335a0a019d985b76420120f9567a400
shanker <uperbacker@gmail.com>
2024-07-30 22:16:06 +0800

    relegate useEffect hook from Home.js to new custom hook useFetch

---
+++ src/useFetch.js
@@ -0,0 +1,32 @@
+import {useState, useEffect} from 'react';
+
+const useFetch = (url) => {
+    const [data, setData] = useState(null); //change from blog to data to make cust. hook universal
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {   
+        setTimeout(() => {
+                fetch(url)
+                .then(res => {
+                    if(!res.ok) {
+                        throw Error('could not fetch the data for that resource')
+                    }
+                    return res.json()
+                })
+                .then((data) => {
+                    setData(data);
+                    setIsLoading(false);
+                    setError(null);
+                })
+                .catch(err => {
+                    setIsLoading(false);
+                    setError(err.message);
+                })
+        }, 1000)
+    }, [url]); //when url is changed, rerun the fn for the new url
+
+    return {data, isLoading, error}
+}
+
+export default useFetch;
