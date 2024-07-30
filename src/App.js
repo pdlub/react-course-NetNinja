df5b6349efde21f8c81ea17b066366ef89ca0c6a
shanker <uperbacker@gmail.com>
2024-07-30 22:31:46 +0800

    install react router v6, wrap App div within Router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Navbar from './Navbar';
 import Home from './Home';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 
 function App() {
@@ -9,13 +10,16 @@ function App() {
 
 
   return (
-    <div className="App">
-      <Navbar/>
-      <div className="content">
-        <Home/>
-
+    <Router>
+      <div className="App">
+        <Navbar/>
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+          </Routes>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
