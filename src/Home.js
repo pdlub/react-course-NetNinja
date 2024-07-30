7a8fb28ac751e9f956fac8cd25eedcb7bcb1b678
shanker <uperbacker@gmail.com>
2024-07-30 21:53:16 +0800

    add error catching functionality to home.js

--- src/Home.js
+++ src/Home.js
@@ -3,26 +3,35 @@ import BlogList from './BlogList';
 
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
-    
-
     const [name, setName] = useState('mario');
     const [isLoading, setIsLoading] = useState(true);
 
+    const [error, setError] = useState(null);
+
     useEffect(() => {   
         setTimeout(() => {
-            fetch('http://localhost:8000/blogs')
-            .then(res => {
-                return res.json()
-            })
-            .then((data) => {
-                setBlogs(data);
-                setIsLoading(false);
-            });
+                fetch('http://localhost:8000/blogs')
+                .then(res => {
+                    if(!res.ok) {
+                        throw Error('could not fetch the data for that resource')
+                    }
+                    return res.json()
+                })
+                .then((data) => {
+                    setBlogs(data);
+                    setIsLoading(false);
+                    setError(null);
+                })
+                .catch(err => {
+                    setIsLoading(false);
+                    setError(err.message);
+                })
         }, 1000)
     }, []);
 
     return ( 
         <div className="home">
+            {error && <div>{error}</div>}
             {isLoading && <div>Loading...</div>}
             {blogs && <BlogList blogs = {blogs} title = {'All blogs'}/>}
         </div>
