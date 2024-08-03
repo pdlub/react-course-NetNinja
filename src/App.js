46aa9922833d4eaf657bbe3d09b71b5729adf981
shanker <uperbacker@gmail.com>
2024-08-03 22:56:40 +0800

    create Create.js, include path to /create in routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Navbar from './Navbar';
 import Home from './Home';
+import Create from './Create';
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 
@@ -16,6 +17,7 @@ function App() {
         <div className="content">
           <Routes>
             <Route path="/" element={<Home />}></Route>
+            <Route path="/create" element={<Create />}></Route>
           </Routes>
         </div>
       </div>
